010c456b1d9bf97af1ddcfdfc1a91355d8a2a8b0
23f3000080 <23f3000080@ds.study.iitm.ac.in>
2026-07-28 09:54:49 +0000

    correct backend url

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,4 +1,4 @@
-const FALLBACK_BASE_URL = 'http://127.0.0.1:5000'
+const FALLBACK_BASE_URL = 'https://educationalsociety.duckdns.org'
 
 export function getApiBaseUrl() {
   return import.meta.env.VITE_API_BASE_URL || FALLBACK_BASE_URL
